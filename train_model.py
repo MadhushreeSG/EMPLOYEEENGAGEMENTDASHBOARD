12d21e96e9ba766979ab0f81d6f0639fc1463f25
MadhushreeSG <sgmadhushree@gmail.com>
2026-03-15 12:55:36 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,18 +2,18 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-# Load dataset
-df = pd.read_csv("Palo Alto Networks.csv")
+# Load your dataset
+df = pd.read_csv("Palo Alto Networks.csv")  # Make sure CSV is in same folder
 
 # Features and target
 X = df[["JobSatisfaction","EnvironmentSatisfaction","WorkLifeBalance","Overtime","YearsAtCompany"]]
 y = df["BurnoutLevel"]
 
-# Train model
+# Train the model
 model = RandomForestClassifier()
 model.fit(X, y)
 
-# Save the trained model
+# Save model as pickle
 with open("employee_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
